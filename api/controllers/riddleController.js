d02a15a6906ef2a86a7384e94cc68ffe3eb6f335
brendan schwarz <bschwarz890@gmail.com>
2019-07-15 13:17:32 -0700

    add get riddles routes

--- api/controllers/riddleController.js
+++ api/controllers/riddleController.js
@@ -5,17 +5,19 @@ const mongoose = require('mongoose');
 const Riddle = mongoose.model('Riddle');
 
 exports.createRiddle = function(req, res) {
-    const content = {
-        answer: req.body.answer.split(','),
-        description: req.body.description,
-        author: req.body.author
-    }
-    
-    const riddle = new Riddle(content);
+    const riddle = new Riddle(req.body);
 
     riddle.save(function(err, riddle) {
         err
             ? res.send(err)
             : res.json(riddle);
+    })
+}
+
+exports.getRiddles = function(req, res) {
+    Riddle.find({}, function(err, riddles) {
+        err
+            ? res.send(err)
+            : res.json(riddles);
     })
 }
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -14,6 +14,7 @@ const registerRoutes = function(app) {
 
     app.route('/riddles')
         .post(riddleContoller.createRiddle)
+        .get(riddleContoller.getRiddles)
 
     app.route('/answers')
         .post(answerController.createAnswer)
